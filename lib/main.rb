a316e2b8a66e725ac68b8308074f113685ebe782
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-02-05 12:10:22 -0300

    add main class with available items

---
+++ lib/main.rb
@@ -0,0 +1,9 @@
+class Main
+  AVAILABLE_ITEMS = [
+    { description: 'book', price: 12.49 },
+    { description: 'music CD', price: 14.99 },
+    { description: 'chocolate bar', price: 0.85 },
+    { description: 'imported box of chocolates', price: 10.00 },
+    { description: 'imported bottle of perfume', price: 47.50 }
+  ].freeze
+end
