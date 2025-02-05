f4527721dcc011b52dbe3239bb6f803f6d0aa99b
Luisfelipedev27 <luisfelipe1582@hotmail.com>
2025-02-05 14:30:10 -0300

    build invoice details

--- lib/invoice.rb
+++ lib/invoice.rb
@@ -5,7 +5,24 @@ class Invoice
     @final_total = 0.0
   end
 
+  def details
+    @details = build_receipt
+  end
+
   private
+
+  def build_receipt
+    receipt_message = item_details
+    receipt_message <<< "Sales Taxes: #{format('%.2f', @accumulated_taxes)}\n"
+    receipt_message << "Total: #{format('%.2f', @final_total)}"
+  end
+
+  def item_details
+    @items.reduce('') do |output, item|
+      update_totals(item)
+      output << format_item_details(item)
+    end
+  end
 
   def update_totals(item)
     @accumulated_taxes += item.total_taxes
